583977179c514cf9346982222d6bf81a235679f4
Vu Le Nam <namvule@hotmail.com>
2018-10-04 10:09:24 +0900

    update models

--- ticket/models.py
+++ ticket/models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
+  disabled = db.Column(db.Boolean, default=False)
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
@@ -24,12 +25,14 @@ class User(UserMixin, db.Model):
     return User.query.get(int(id))
 
 class Ticket(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  buyer_email = db.Column(db.String(128), index=True, unique=True)
+  unique_id = db.Column(db.String(32), primary_key=True)
+  buyer_email = db.Column(db.String(128))
   buyer_tel = db.Column(db.String(16))
   quanlity = db.Column(db.Integer)
   amount = db.Column(db.Integer)
-  deleted = db.Column(db.Boolean)
+  paid = db.Column(db.Boolean, default=False)
+  paid_at = db.Column(db.DateTime())  
+  deleted = db.Column(db.Boolean, default=False)
   deleted_at = db.Column(db.DateTime())
   created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
   updated_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -40,4 +43,4 @@ class Ticket(db.Model):
   def __repr__(self):
     return 'id:{}, buyer_email:{}, buyer_tel:{}, quanlity:{}, amount:{}>'.format(self.id, self.buyer_email, self.buyer_tel, self.quanlity, self.amount)
   def __str__(self):
-    return 'id:{}, buyer_email:{}, buyer_tel:{}, quanlity:{}, amount:{}>'.format(self.id, self.buyer_email, self.buyer_tel, self.quanlity, self.amount)
+    return '{},{},{},{},{}'.format(self.unique_id, str(self.buyer_tel).ljust(16), str(self.quanlity).ljust(2), self.paid, self.note.ljust(9))
