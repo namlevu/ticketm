50590583451d302a340cb085cde4c6e42df2d9b1
namlevu <namvule@hotmail.com>
2018-10-04 23:00:56 +0900

    for init admin acc

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import os
+
 from ticket import app
+# On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
+# When running this app on the local machine, default the port to 8000
+port = int(os.getenv('PORT', 8000))
+os.system("init_db.py")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port="80")
+  app.run(host='0.0.0.0',port=port)
